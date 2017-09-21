f32374214188324bd3ed6bf1919e9d660312cff3
Dejesusj9863 <31896126+Dejesusj9863@users.noreply.github.com>
2017-09-21 10:10:19 -0400

    Add files via upload
    Calculate the Distance Traveled for the three given times. (6, 10, 15)

---
+++ M2HW1_DistanceTraveled_DeJesusJose.py
@@ -0,0 +1,18 @@
+# CTI-110
+# M2HW1 - Distance Traveled 
+# Jose De Jesus
+# 09/14/17
+# Write a program that calculates distance traveled.
+
+
+
+# Variables
+SPEED = 70
+DistanceAfter6 = SPEED * 6
+DistanceAfter10 = SPEED * 10
+DistanceAfter15 = SPEED * 15
+
+#printed out statements
+print("The distance, in miles, the car will travel in 6 hours:", DistanceAfter6)
+print("The distance, in miles, the car will travel in 10 hours:", DistanceAfter10)
+print("The distance, in miles, the car will travel in 15 hours:", DistanceAfter15)
